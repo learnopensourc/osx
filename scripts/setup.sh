ad7a13e36a1296f434b6894b2ba63fa4473565c1
Brooke Kuhlmann <brooke@alchemists.io>
2016-09-30 09:20:45 -0600

    Updated setup script to use HTTPS protocol.
    - Improves download security.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,7 +19,7 @@ chsh -s /usr/local/bin/bash
 printf "%s\n" "---" > "$HOME/.gemrc"
 printf "%s\n" "gem: --no-document" >> "$HOME/.gemrc"
 printf "%s\n" "$MRI" > "$HOME/.ruby-version"
-git clone git://github.com/bkuhlmann/ruby_setup.git
+git clone https://github.com/bkuhlmann/ruby_setup.git
 (
   cd ruby_setup
   git checkout $REPO_RUBY_SETUP
@@ -29,10 +29,10 @@ git clone git://github.com/bkuhlmann/ruby_setup.git
 rm -rf ruby_setup
 
 # Go
-install_git_project "git://github.com/bkuhlmann/go_setup.git" $REPO_GO_SETUP "go_setup" "./run.sh i"
+install_git_project "https://github.com/bkuhlmann/go_setup.git" $REPO_GO_SETUP "go_setup" "./run.sh i"
 
 # NPM
-install_git_project "git://github.com/bkuhlmann/npm_setup.git" $REPO_NPM_SETUP "npm_setup" "./run.sh i"
+install_git_project "https://github.com/bkuhlmann/npm_setup.git" $REPO_NPM_SETUP "npm_setup" "./run.sh i"
 
 # Pow
 curl get.pow.cx | sh
@@ -41,9 +41,9 @@ curl get.pow.cx | sh
 if [ ! -e "/usr/bin/sublime" ]; then
   sudo ln -sv "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
 fi
-install_git_project "git://github.com/bkuhlmann/sublime_text_setup.git" $REPO_SUBLIME_TEXT_SETUP "sublime_text_setup" "./run.sh i"
+install_git_project "https://github.com/bkuhlmann/sublime_text_setup.git" $REPO_SUBLIME_TEXT_SETUP "sublime_text_setup" "./run.sh i"
 
 # Dotfiles
 rm -f $HOME/.bash_profile
-install_git_project "git://github.com/bkuhlmann/dotfiles.git" $REPO_DOTFILES "dotfiles" "./run.sh i"
+install_git_project "https://github.com/bkuhlmann/dotfiles.git" $REPO_DOTFILES "dotfiles" "./run.sh i"
 source $HOME/.bashrc
